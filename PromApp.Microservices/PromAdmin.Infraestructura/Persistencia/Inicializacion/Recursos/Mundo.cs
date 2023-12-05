ddd5bf0dcd135d4671c19174f9251f1bba95215d
Jeyson Esteban Parra Bedoya <COX0000732@prosegur.com>
2023-12-05 09:58:09 -0500

    - Actualización clase con clean code

--- PromApp.Microservices/PromAdmin.Infraestructura/Persistencia/Inicializacion/Recursos/Mundo.cs
+++ PromApp.Microservices/PromAdmin.Infraestructura/Persistencia/Inicializacion/Recursos/Mundo.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace PromAdmin.Infraestructura.Persistencia.Inicializacion.Recursos;
 
 public class Mundo
@@ -6,22 +8,24 @@ public class Mundo
 }
 public class City
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
 }
 
 public class Country
 {
-    public string Name { get; set; }
-    public string Iso2 { get; set; }
-    public string Iso3 { get; set; }
-    public string Phone_code { get; set; }
-    public string Currency { get; set; }
-    public List<State> States { get; set; }
+    public string? Name { get; set; }
+    public string? Iso2 { get; set; }
+    public string? Iso3 { get; set; }
+    [JsonProperty("phonecode")]
+    public string? PhoneCode { get; set; }
+    public string? Currency { get; set; }
+    public List<State>? States { get; set; }
 }
 
 public class State
 {
-    public string Name { get; set; }
-    public string State_code { get; set; }
-    public List<City> Cities { get; set; }
+    public string? Name { get; set; }
+    [JsonProperty("state_code")]
+    public string? StateCode { get; set; }
+    public List<City>? Cities { get; set; }
 }
